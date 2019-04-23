f4a1abe071703faf13610cc801421983e4443b94
Chris Chadillon <chris@chadillon.com>
2019-04-23 17:40:57 -0400

    Added some cells

--- APIStore/Controllers/SearchCollectionViewController.swift
+++ APIStore/Controllers/SearchCollectionViewController.swift
@@ -20,5 +20,18 @@ class SearchCollectionViewController: UICollectionViewController, UICollectionVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellID")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let theCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellID", for: indexPath)
+        theCell.backgroundColor = .red
+        return theCell
     }
 }
+
